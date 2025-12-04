6fdfdb7a0e00a0018cbc5afd7be5be77e9c568c2
Danny Ben Shitrit <db@dannyben.com>
2025-12-04 22:32:00 +0000

    add tests

--- src/commands/templates/list.sh
+++ src/commands/templates/list.sh
@@ -1,6 +1,7 @@
 if [[ ! -d "$templates_dir" ]]; then
   log error "templates dir not found: $(blue "$templates_dir")"
   log info "change templates dir using $(blue REPLI_TEMPLATES_DIR)"
+  return 1
 fi
 
 log debug "templates dir: $(blue "$templates_dir")"

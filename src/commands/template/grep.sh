3248c24757b36609200f80e0312dc2fb3fad4fea
Danny Ben Shitrit <db@dannyben.com>
2025-12-10 10:23:36 +0000

    ci: fix grep order

--- src/commands/template/grep.sh
+++ src/commands/template/grep.sh
@@ -4,7 +4,14 @@ echo "Templates: $(blue "$templates_dir")"
 echo "Search:    $(green "$search")"
 echo
 
-grep -Ril --include="*.yaml" "$search" "$templates_dir" |
-  sed -e "s|^$templates_dir/||" -e 's/\.yaml$//'
+(
+  cd "$templates_dir" >/dev/null || return 1
 
-
+  find . \
+    -type f -name '*.yaml' \
+    -printf '%P\0' |
+    xargs -0 grep -ilZ "$search" |
+    sed -z 's/\.yaml$//' |
+    sort -zV |
+    tr '\0' '\n'
+)
